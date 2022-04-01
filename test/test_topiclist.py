3687e41c28ddbde5f5e8c3b898a5f1b778f8bd36
zhangzhihui19950725 <1872135605@qq.com>
2022-04-01 09:05:59 +0800

    更新用例

---
+++ test/test_topiclist.py
@@ -0,0 +1,57 @@
+import minium
+
+from test.common import delay
+
+class TestTopicList(minium.MiniTest):
+
+    """
+    话题列表页
+    """
+
+    def setUp(self) -> None:
+        self.app.navigate_to("/page/taofangquan/huati/huatiList?city=qz")
+        delay(2)
+        self.app.get_current_page()
+        print("setUp!!!!")
+
+    def test_click_topictitle(self):
+        """
+        话题列表，点击话题标题
+        :return:
+        """
+        e = self.page.get_element('view[class="title"]')
+        e.tap()
+        delay(2)
+
+    def test_click_PK(self):
+        """
+        话题列表，点击pk话题pk按钮
+        :return:
+        """
+        if self.page.get_element('view[class="Pk--button"]'):
+            e = self.page.get_element('view[class="Pk--button"]')
+            e.tap()
+            delay(1)
+        else:
+            print("没有可点击的Pk按钮")
+
+    def test_click_join(self):
+        """
+        话题列表，点击普通话题“查看详情”按钮
+        :return:
+        """
+        e = self.page.get_element('view[class="join"]')
+        e.tap()
+        delay(2)
+
+    def test_click_share(self):
+        """
+        话题列表，点击分享按钮
+        :return:
+        """
+        e = self.page.get_element('button[class="newHouseRfixed-share"]')
+        e.tap()
+        delay(2)
+        # self.native.handle_modal("取消")
+
+    
