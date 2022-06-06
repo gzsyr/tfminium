906a96ee026942122b9bfccc17b15c6d813e20db
Jennifer <zhusuyun@vip.house365.com>
2022-06-06 15:40:58 +0800

    更新名字

---
+++ test/test_commentdetail.py
@@ -0,0 +1,69 @@
+# add by zsy
+from test.test_base import TestBase
+
+
+class TestCommentDetail(TestBase):
+    """
+    帖子的评论详情页
+    """
+    def setUp(self) -> None:
+        self.page_name = "/page/taofangquan/tieziCommentDetail/tieziCommentDetail?city=qz&pid=47170&postsid=12746"
+        self.switch = False
+        super(TestCommentDetail, self).setUp()
+        print("TestCommentDetail setup test")
+
+    def test_click_open_arrow(self):
+        """
+        点击向下箭头，展示全部内容
+        :return:
+        """
+        self.page.get_element('view[class="open"]').tap()
+
+    def test_click_tiezi_dianzan(self):
+        """
+        帖子的点赞操作
+        :return:
+        """
+        self.page.get_element('view[class="laud-btn"]').tap()
+
+    def test_click_comments_avator(self):
+        """
+        点击主评论的头像
+        :return: 
+        """
+        self.page.get_element('image[class="commentList--avator"]').tap()
+
+    def test_click_comments_dianzai(self):
+        """
+        主评论的点赞操作
+        :return:
+        """
+        self.page.get_element('view[class="commentList--laud-btn"]').tap()
+
+    def test_click_addgroupimg(self):
+        """
+        点击左下角的“加群”icon
+        :return:
+        """
+        self.page.get_element('image[class="addgroupimg"]').tap()
+
+    def test_click_share(self):
+        """
+        点击左下角“分享”icon
+        :return:
+        """
+        self.page.get_element('button[class="detail-fix-share"]').tap()
+
+    def test_click_returnPl(self):
+        """
+        点击悬浮“返回评论”icon
+        :return:
+        """
+        self.page.get_element('image[class="returnPl"]').tap()
+
+    def test_get_page_wxml(self):
+        """
+        获取当前页面的wxml，并打印
+        :return:
+        """
+        print(self.page.get_element('page'))
