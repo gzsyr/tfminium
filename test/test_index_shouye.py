ae4768cca0c1c677fb5a7bd8640fa72755b6eafc
Jennifer <zhusuyun@vip.house365.com>
2022-07-22 11:09:44 +0800

    更新代码

--- test/test_index_shouye.py
+++ test/test_index_shouye.py
@@ -2,7 +2,26 @@
 from test.test_base import TestBase
 
 
-class TestIndex(TestBase):
+class TestIndexShouye(TestBase):
     """
     淘房小程序首页
     """
+    def setUp(self) -> None:
+        self.page_name = "/page/index/index"
+        self.switch = True
+        super(TestIndexShouye, self).setUp()
+        print("TestIndexShouye setup test")
+
+    def test_click_banner_one(self):
+        """
+        点击首页联板广告第一张
+        :return:
+        """
+        self.page.get_element('image[class="bannerTwo-img index_banner"]').click()
+
+    def test_goto_newhouse(self):
+        """
+        点击功能入口：第一个，新房
+        :return:
+        """
+        self.page.get_element('view[class="myiconTwoLi index_xf"]').click()
