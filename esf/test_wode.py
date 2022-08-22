0a9280b2e7dc81906713e314eb1c7e2d60be1dec
zhaobaorong <397047784@qq.com>
2022-08-22 19:44:59 +0800

    Signed-off-by: zhaobaorong <397047784@qq.com> 8.22

--- esf/test_wode.py
+++ esf/test_wode.py
@@ -17,6 +17,54 @@ class Testesfwd(TestBase):
         super(Testesfwd, self).setUp()
         print("Testesfwd setup")
 
+    @file_data('./test_func_weizhi.yml')
+    def test_select_qy(self, **kargs):
+        """
+        筛选位置-区域
+        :return:
+        """
+        e4 = self.page.get_element('view[class="pa clear"]')
+        e4.tap()
+        e = self.page.get_element("view", inner_text=kargs['district'])
+        e.tap()
+        e1 = self.page.get_element("view", inner_text=kargs['searchone'])
+        e1.tap()
+        e2 = self.page.get_element("view", inner_text=kargs['searchtwo'])
+        e2.tap()
+        e3 = self.page.get_element("view", inner_text=kargs['searchthree'])
+        e3.tap()
+        delay(2)
+
+    @file_data('./test_func_zongjia.yml')
+    def test_select_zjzdy(self, **kwargs):
+        """
+        筛选总价自定义
+        :return:
+        """
+        e3 = self.page.get_element('view[class="pa clear"]')
+        e3.tap()
+        e = self.page.get_element("view", inner_text=kwargs['zdy'])
+        e.tap()
+        self.page.get_element("input", inner_text="最低价").input(kwargs['nameone'])
+        self.page.get_element("input", inner_text="最高价").input(kwargs['nametwo'])
+        e1 = self.page.get_element('view[class="price--text_center price--confirm"]')
+        e1.tap()
+        delay(2)
+
+    @file_data('./test_func_zongjia.yml')
+    def test_select_fx(self, **kwargs):
+        """
+        筛选房型
+        :return:
+       """
+        self.page.get_element('view[class="pa clear"]').tap()
+        e = self.page.get_element("view", inner_text=kwargs['fx'])
+        e.tap()
+        self.page.get_element('text', inner_text=kwargs['namethree']).tap()
+        self.page.get_element('text', inner_text=kwargs['namefour']).tap()
+        self.page.get_element('text', inner_text="确定").tap()
+        delay(2)
+
     @file_data('./test_func_zongjia.yml')
     def test_select_gdqd(self, **kwargs):
         """
@@ -47,15 +95,14 @@ class Testesfwd(TestBase):
         self.page.get_element('text', inner_text="确定").tap()
         delay(2)
 
-    """
+
     @file_data('./test_func_zongjia.yml')
     def test_select_px(self, **kwargs):
-        
+        """
         排序
         :return:
-      
+      """
         self.page.get_element('view[class="pa clear"]').tap()
         self.page.get_element("view", inner_text=kwargs['paixu']).tap()
         self.page.get_element("text", inner_text=kwargs['paixuone']).tap()
         delay(2)
-        """
