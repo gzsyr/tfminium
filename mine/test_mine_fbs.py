315b6fc323b16b34ec6d37c5851d24cb51e8e2b5
Jennifer <zhusuyun@vip.house365.com>
2022-09-05 11:10:34 +0800

    新增用例

---
+++ mine/test_mine_fbs.py
@@ -0,0 +1,22 @@
+# add by zsy
+from base.test_base import TestBase
+
+
+class TestMineFbs(TestBase):
+    """
+    我的问答页
+    """
+    def setUp(self) -> None:
+        self.page_name = '/fbs/mine/mine?city=qz'
+        self.switch = False
+        self.classname = self.__class__.__name__
+        super(TestMineFbs, self).setUp()
+
+    def test_goto_ask_detail(self):
+        """
+        我的问答页面，点击第一个问答，进入问答详情页
+        """
+        self.page.get_element('view[class="question-text ml30"]').click()
+
+        self.verifyPageName('/fbs/detail/detail')
+        self.get_capture()
