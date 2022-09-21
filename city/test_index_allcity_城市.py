90b282a85357e84af021e1589bb9b765d6f3adf1
Jennifer <zhusuyun@vip.house365.com>
2022-09-21 17:07:11 +0800

    更新

---
+++ city/test_index_allcity_城市.py
@@ -0,0 +1,36 @@
+# add by zsy
+from base.test_base import TestBase
+
+
+class TestAllcity(TestBase):
+    def setUp(self) -> None:
+        self.page_name = "/page/index/city"
+        self.switch = False
+        self.classname = self.__class__.__name__
+        super(TestAllcity, self).setUp()
+        print("TestAllcity setup atest")
+
+    def test_select_qz_选择泉州(self):
+        """
+        选择站点：泉州
+        :return:
+        """
+        while self.get_newcity() != '泉州':
+            print('当前城市不是泉州，需要切换')
+            self.page.get_element('text[class="hot-city"]', inner_text="泉州").tap()
+            self.delay(1)
+        self.app.go_home()
+        self.get_screenshot()
+
+    def test_select_nj_选择南京(self):
+        """
+        选择站点：南京
+        :return:
+        """
+        while self.get_newcity() != '南京':
+            print('当前城市不是南京，需要切换')
+            self.page.get_element('text[class="hot-city"]', inner_text="南京").tap()
+            self.delay(1)
+        self.app.go_home()
+        self.get_screenshot()
+
