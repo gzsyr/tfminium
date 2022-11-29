35ac0cb619c08c8e36dec2a058f73297cf1a02b0
Jennifer <zhusuyun@vip.house365.com>
2022-11-29 15:31:00 +0800

    优化

--- tfq/test_tfq_commentdetail_帖子评论详情.py
+++ tfq/test_tfq_commentdetail_帖子评论详情.py
@@ -19,7 +19,7 @@ class TestTfqCommentDetail(TestBase):
         """
         帖子的评论详情页，点击向下箭头，展示全部内容
         """
-        self.page.get_element('view[class="open"]').tap()
+        self.find_element('view[class="open"]').tap()
 
         self.get_screenshot()
 
@@ -34,7 +34,7 @@ class TestTfqCommentDetail(TestBase):
         """
         帖子的评论详情页，点击主评论的头像
         """
-        self.page.get_element('image[class="commentList--avator"]').tap()
+        self.find_element('image[class="commentList--avator"]').tap()
 
         self.verifyPageName('/page/newhouse/zygw/detail')
         self.get_screenshot()
@@ -52,7 +52,7 @@ class TestTfqCommentDetail(TestBase):
         帖子的评论详情页，点击左下角的“加群”icon
         """
         try:
-            self.page.get_element('image[class="addgroupimg"]').tap()
+            self.find_element('image[class="addgroupimg"]').tap()
         except minium.MiniElementNotFoundError:
             print('没有加群广告')
 
@@ -62,7 +62,7 @@ class TestTfqCommentDetail(TestBase):
         """
         帖子的评论详情页，点击左下角“分享”icon帖子的评论详情页，
         """
-        self.page.get_element('button[class="detail-fix-share"]').tap()
+        self.find_element('button[class="detail-fix-share"]').tap()
         self.delay(1)
         self.verifyByScreenshot('tfq/test_click_post_share.png')
 
@@ -70,7 +70,7 @@ class TestTfqCommentDetail(TestBase):
         """
         帖子的评论详情页，点击悬浮“返回评论”icon
         """
-        self.page.get_element('image[class="returnPl"]').tap()
+        self.find_element('image[class="returnPl"]').tap()
 
         self.verifyPageName('/page/taofangquan/tieziDetail/tieziDetail')
         self.get_screenshot()
@@ -79,7 +79,7 @@ class TestTfqCommentDetail(TestBase):
         """
         帖子的评论详情页，点击最下面一行的评论入口
         """
-        self.page.get_element('view[class="detail-fix-input"]').tap()
+        self.find_element('view[class="detail-fix-input"]').tap()
 
         self.get_screenshot()
 
@@ -87,6 +87,6 @@ class TestTfqCommentDetail(TestBase):
         """
         帖子的评论详情页，帖子评论详情页，点击“查看完整图片”按钮
         """
-        self.page.get_element('view[id="newHouseBanner-ckmore"]').tap()
+        self.find_element('view[id="newHouseBanner-ckmore"]').tap()
 
         self.get_screenshot()
