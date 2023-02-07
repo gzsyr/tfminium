3a6d35df13c45d3007572737d4a22d921ef9f36a
Jennifer <zhusuyun@vip.house365.com>
2023-02-07 10:41:06 +0800

    优化

--- logout/test_gfzl_未登录_购房资料.py
+++ logout/test_gfzl_未登录_购房资料.py
@@ -12,12 +12,28 @@ class TestLogoutGfzl(TestBase):
         self.classname = self.__class__.__name__
         super(TestLogoutGfzl, self).setUp()
 
-    def test_click_preview_点击文件预览(self):
+    def test_01_click_preview_点击文件预览(self):
         """
         未登录，进入购房百科-购房资料页面，点击预览
         """
         self.find_element('view[class="downBtn disflex disflex-alignitems-center disflex-justifycontent-center"]').tap()
 
         # self.verifyPageName('/page/index/login')
+        self.get_screenshot()
+
+    def test_02_click_zygw_im_点击置业顾问IM(self):
+        """
+        V6.29.X: 1004929，点击置业顾问IM
+        """
+        self.find_element('view[class="link-button im"]').tap()
+
+        self.get_screenshot()
+
+    def test_03_click_zygw_im_点击置业顾问电话(self):
+        """
+        V6.29.X: 1004929，点击置业顾问电话
+        """
+        self.find_element('view[class="link-button call"]').tap()
+
         self.get_screenshot()
 
