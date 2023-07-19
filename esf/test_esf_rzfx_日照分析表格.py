2d54dc98908ce651649fd6bc49adb96136efcf79
zhusuyun <348299953@qq.com>
2023-07-19 16:54:31 +0800

    update639

---
+++ esf/test_esf_rzfx_日照分析表格.py
@@ -0,0 +1,186 @@
+# add by zsy
+from base.test_base import TestBase
+
+
+class TestEsfRzfxBg(TestBase):
+    """
+    日照分析表格页面
+    """
+    def setUp(self) -> None:
+        self.page_name = '/page/newhouse/rizhaofenxi/rizhaofenxi?city=nj&esfId=10017892&tab=1'
+        self.switch = False
+        self.classname = __class__.__name__
+        super(TestEsfRzfxBg, self).setUp()
+
+
+    def test_goto_3d(self):
+        """
+        点击 进入3D 模型
+        """
+        self.find_element('view[class="icon tfFlex tfAlignC icon_esf_1"]').tap()
+
+        self.get_screenshot()
+        self.verifyPageName('/page/newhouse/rizhaofenxi/rizhao3d')
+
+    def test_click_节气楼栋(self):
+        """
+        V6.38.x: 点击 大寒  +  2 栋楼
+        """
+        self.find_element('view[class="jqItem"][data-id="6"]').tap()
+        self.find_element('view[class="ldItem"][data-id="6"]').tap()
+
+        self.get_screenshot()
+
+    def test_tab_底部控件收起展开(self):
+        """
+        V6.38.X: 点击 底部控件 展开收起操作
+        """
+        self.find_element('view[class="lpBottomBar--popup_icon"]').tap()
+        self.delay(2)
+        self.get_screenshot('展开截图')
+        self.find_element('view[class="lpBottomBar--popup_icon lpBottomBar--open_icon"]').tap()
+        self.delay(1)
+        self.get_screenshot('收起截图')
+
+    def click_hx(self):
+        """
+        点击 户型
+        """
+        # 点击主力户型
+        self.find_element('view[class="lpBottomBar--tab"][data-index="1"]').tap()
+
+    def test_zzhx_选择两室查看户型图(self):
+        """
+        V6.38.X: 点击 主力户型 选择 二室 查看户型图
+        """
+        # 点击主力户型
+        self.click_hx()
+        self.delay(2)
+        # 点击 二室
+        self.find_element('view[class="lpBottomBar--hxtab"][data-key="2"]').tap()
+        self.get_screenshot('选择二室的截图')
+
+        self.find_element('image[class="lpBottomBar--hxImg"]').tap()
+        self.get_screenshot('点击户型图的截图')
+
+    def test_hx_咨询在售(self):
+        """
+        V6.38.X: 点击 小区户型，点击 咨询底价
+        """
+        # 点击主力户型
+        self.click_hx()
+        # 点击咨询底价
+        self.find_element('view[class="lpBottomBar--tfFlex lpBottomBar--tfAlignC lpBottomBar--tfFlexC lpBottomBar--consult_hx"]').tap()
+        self.delay(3)
+        self.get_screenshot()
+
+    def test_hx_更多(self):
+        """
+        V6.37.X: 点击 主力户型，点击 更多
+        """
+        # 点击小区户型
+        self.click_hx()
+        # 点击更多
+        self.find_element('view[class="lpBottomBar--lpmore"]', inner_text="更多").tap()
+
+        self.get_screenshot()
+        self.verifyPageName('/esf/village/pages/detail/detailHxImage/detailHxImage')
+
+    def click_xqfy(self):
+        """
+        点击 小区房源
+        """
+        self.find_element('view[class="lpBottomBar--tab"][data-index="2"]').tap()
+
+    def test_xqfy_在租更多(self):
+        """
+        V6.38.X: 点击 小区房源，点击 更多
+        """
+        self.click_xqfy()
+
+        # 点击 在租
+        self.find_element('view[class="lpBottomBar--fytab"][data-type="2"]').tap()
+
+        # 点击 更多
+        self.find_element('view[class="lpBottomBar--xq_consult_hx"]').tap()
+
+        self.get_screenshot()
+        self.verifyPageName('/esf/sell/rent/list/list')
+
+    def test_xqfy_在租房源详情(self):
+        """
+        V6.38.X: 点击 小区房源，点击 整租
+        """
+        self.click_xqfy()
+
+        # 点击 在租
+        self.find_element('view[class="lpBottomBar--fytab"][data-type="2"]').tap()
+
+        # 点击房源
+        self.find_element('view[class="item--line_1 item--title"]').tap()
+
+        self.get_screenshot()
+        self.verifyPageName('/esf/sell/rent/detail/detail')
+
+    def test_xqfy_切换租售进入出售房源详情(self):
+        """
+        V6.38.X: 点击  小区房源，点击在租  在售，点击房源进入详情
+        """
+        self.click_xqfy()
+
+        # 点击 在租
+        self.find_element('view[class="lpBottomBar--fytab"][data-type="2"]').tap()
+
+        # 点击 在售
+        self.find_element('view[class="lpBottomBar--fytab"][data-type="1"]').tap()
+
+        # 点击房源
+        self.find_element('image[class="sell-item--img"]').tap()
+
+        self.get_screenshot()
+        self.verifyPageName('/esf/sell/pages/detail/detail')
+
+    def click_lp(self):
+        """
+        点击 小区信息
+        """
+        self.find_element('view[class="lpBottomBar--tab lpBottomBar--tabActive"][data-index="0"]').tap()
+
+    def test_xq_进入小区详情(self):
+        """
+        V6.38.X: 点击 小区信息，进入小区详情页
+        """
+        self.click_lp()
+
+        self.find_element('view[class="lpBottomBar--lpmore"]', inner_text='查看详情').tap()
+
+        self.get_screenshot()
+        self.verifyPageName('/esf/village/pages/detail/detail')
+
+    def test_xq_咨询近期成交(self):
+        """
+        V6.38.X: 点击 小区信息，咨询近期成交
+        """
+        self.click_lp()
+
+        self.find_element('view[class="lpBottomBar--xq_consult_imicon_hx"]').tap()
+
+        self.get_screenshot()
+
+    def test_z_在线咨询(self):
+        """
+        V6.38.X: 点击 在线咨询
+        """
+        self.find_element('view[class="contact--center contact--pr contact--msg"]').tap()
+
+        self.delay(3)
+        self.get_screenshot()
+
+    def test_zz_打电话(self):
+        """
+        V6.38.X: 点击 拨打电话
+        """
+        self.find_element('view[class="contact--center contact--tel"]').tap()
+
+        self.get_screenshot()
+
