6ce86683c901ec6cd6cff8df0d2650d5ba753d84
zhusuyun <348299953@qq.com>
2023-08-03 15:18:12 +0800

    update640

--- logout/test_gfzl_未登录_购房资料.py
+++ logout/test_gfzl_未登录_购房资料.py
@@ -7,7 +7,7 @@ class TestLogoutGfzl(TestBase):
     未登录  购房资料页面测试用例
     """
     def setUp(self) -> None:
-        self.page_name = '/page/tools/ziliaomoban/ziliaomoban?city=qz'
+        self.page_name = '/page/tools/ziliaomoban/ziliaomoban?id=&type=xf&city=qz&navTitle=%E8%B4%AD%E6%88%BF%E8%B5%84%E6%96%99'
         self.switch = False
         self.classname = self.__class__.__name__
         super(TestLogoutGfzl, self).setUp()
@@ -16,7 +16,7 @@ class TestLogoutGfzl(TestBase):
         """
         未登录，进入购房百科-购房资料页面，点击预览
         """
-        self.find_element('view[class="downBtn disflex disflex-alignitems-center disflex-justifycontent-center"]').tap()
+        self.find_element('view[class="tfFlex tfAlignC tfFlexC preview"]').tap()
 
         # self.verifyPageName('/page/index/login')
         self.get_screenshot()
@@ -25,7 +25,7 @@ class TestLogoutGfzl(TestBase):
         """
         V6.29.X: 1004929，点击置业顾问IM
         """
-        self.find_element('view[class="link-button im"]').tap()
+        self.find_element('view[class="flex-1 flex tfAlignC tfFlexC link-button im"]').tap()
 
         self.get_screenshot()
 
@@ -33,7 +33,7 @@ class TestLogoutGfzl(TestBase):
         """
         V6.29.X: 1004929，点击置业顾问电话
         """
-        self.find_element('view[class="link-button call"]').tap()
+        self.find_element('view[class="flex-1 flex tfAlignC tfFlexC link-button call"]').tap()
 
         self.get_screenshot()
 
